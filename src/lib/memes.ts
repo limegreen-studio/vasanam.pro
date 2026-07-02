dd52dd71e42f18f51ee932dddd6e265482c8d90a
bharathbr159-alt <bharathbr159@gmail.com>
2026-07-02 15:45:51 +0530

    Update memes.ts

--- src/lib/memes.ts
+++ src/lib/memes.ts
@@ -5,12 +5,16 @@ export interface MemeTier {
 }
 
 export const MEME_TIERS: MemeTier[] = [
-  { max: 5, img: "/assets/memes/slow.jpg", caption: "Semma slow bro... nadandhu poyiruvom vegama!" },
-  { max: 15, img: "/assets/memes/medium.jpg", caption: "Sari sari, aana innum konjam speed venum." },
-  { max: 35, img: "/assets/memes/good.jpg", caption: "Ithu dhaan bro nalla vibe!" },
-  { max: 40, img: "/assets/memes/fast.jpg", caption: "Adhu thaan mass entry!" },
-  { max: 60, img: "/assets/memes/god.jpg", caption: "Thalaivar level speed. Vera level!" },
-]
+  { max: 5,       img: "assets/memes/slow.jpg",      caption: "Semma slow bro... nadandhu poyiruvom vegama!" },
+  { max: 10,       img: "assets/memes/medium.jpg",    caption: "Sari sari, aana innum konjam speed venum." },
+  { max: 15,       img: "assets/memes/good.jpg",      caption: "Ithu dhaan bro nalla vibe!" },
+  { max: 25,       img: "assets/memes/fast.jpg",      caption: "Adhu thaan speed bro!" },
+  { max: 35,       img: "assets/memes/veryfast.jpg",  caption: "vaa saami!" },
+  { max: 45,      img: "assets/memes/blazing.jpg",   caption: "Fingers on fire!" },
+  { max: 50,      img: "assets/memes/insane.jpg",    caption: "Thalaivar level speed. Vera level!!" },
+  { max: Infinity, img: "assets/memes/god.jpg",       caption: "God mode ON!" },
+];
+
 
 export function getMemeForWpm(wpm: number): MemeTier {
   return MEME_TIERS.find((t) => wpm <= t.max) ?? MEME_TIERS[MEME_TIERS.length - 1]
